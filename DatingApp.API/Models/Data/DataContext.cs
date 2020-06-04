619c8c6047bca82499e1a6722c851b1473619f51
Vadim Tuchila <tuchila.vadim@hotmail.com>
2020-06-03 22:41:08 -0400

    Initial Commit

---
+++ DatingApp.API/Models/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Models.Data
+
+{
+    public class DataContext : DbContext //extends DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) 
+        {
+            public DbSet<Value> Values { get; set; };
+        }
+    }
+}
